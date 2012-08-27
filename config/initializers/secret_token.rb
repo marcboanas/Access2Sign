ef0605ccae725c3fb2fdff50c30a2a3d1848ce53
marc john boanas <marcjohnboanas@gmail.com>
2012-08-27 18:43:56 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Access2sign::Application.config.secret_token = 'e5b4938d86b38d713a8c07637c86608a4c577859d5ff0368b446675aa5ec453183099d85c4c4bd669abbd3a46d4aa53f5a76c6a80b07275bce93ce7d59795d91'
